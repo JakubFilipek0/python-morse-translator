c69ed21186882e8dc903f835aeb82a550eeaee32
Jakub <filijaku@gmail.com>
2024-02-24 19:10:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+string_to_translate = input('Enter your message: ')
+string_to_translate = string_to_translate.upper()
+string_in_morse = ''
+for letter in string_to_translate:
+    if letter in MORSE_CODE_DICT.keys():
+        string_in_morse += MORSE_CODE_DICT[letter]
+        string_in_morse += ' '
+    elif letter == ' ':
+        string_in_morse += ' / '
+print(f'Your text in morse code is {string_in_morse}')
